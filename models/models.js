0521a4611b9ff193ff39cc3e9ba3bd445d6785d1
djones <dwaynejones@pursuit.org>
2023-10-18 19:33:59 -0400

    server data adjustments

--- models/models.js
+++ models/models.js
@@ -1,5 +1,137 @@
 const faker = require('faker');
-const { getRandomAmount, transactionTypes, transactionCategories } = require('../utilities/dataUtilities');
+
+// Define transaction types
+const transactionTypes = ["Deposit", "Invoice", "Withdrawal", "Payment"];
+
+// Define transaction categories with descriptions
+const transactionCategories = [
+  {
+    category: "Basic Household Expenses",
+    descriptions: [
+      "Grocery Shopping",
+      "Rent Payment",
+      "Electricity Bill",
+      "Water Bill",
+      "Internet Subscription",
+      "Home Maintenance",
+    ],
+  },
+  {
+    category: "Child Care and Education",
+    descriptions: [
+      "Child's School Supplies",
+      "Tutoring Services",
+      "Daycare Fees",
+      "College Tuition",
+      "Children's Books",
+    ],
+  },
+  {
+    category: "Utilities",
+    descriptions: [
+      "Gas Bill",
+      "Phone Bill",
+      "Cable TV Subscription",
+      "Trash Collection Fee",
+    ],
+  },
+  {
+    category: "Consumer Credit",
+    descriptions: [
+      "Credit Card Payment",
+      "Personal Loan Repayment",
+      "Installment Plan Payment",
+    ],
+  },
+  {
+    category: "Miscellaneous Expenses",
+    descriptions: [
+      "Medical Expenses",
+      "Charity Donation",
+      "Online Shopping",
+      "Magazine Subscription",
+      "Gift Shop Purchase",
+    ],
+  },
+  {
+    category: "Child-Related Education",
+    descriptions: [
+      "Adult Education Course",
+      "Children's Educational Toys",
+      "Education Software Purchase",
+      "Textbooks for Kids",
+    ],
+  },
+  {
+    category: "Pet-Related Expenses",
+    descriptions: [
+      "Veterinary Visit",
+      "Pet Food Purchase",
+      "Grooming Services",
+      "Pet Supplies",
+    ],
+  },
+  {
+    category: "Coffee Expenses",
+    descriptions: [
+      "Coffee Shop Visit",
+      "Coffee Beans Purchase",
+      "Coffee Machine Purchase",
+    ],
+  },
+  {
+    category: "Entertainment",
+    descriptions: [
+      "Movie Tickets",
+      "Concert Tickets",
+      "Streaming Service Subscription",
+    ],
+  },
+  {
+    category: "Fitness",
+    descriptions: [
+      "Gym Membership",
+      "Yoga Class Fee",
+      "Sports Equipment Purchase",
+    ],
+  },
+  {
+    category: "Income",
+    descriptions: [
+      "Salary Deposit",
+      "Freelance Earnings",
+      "Rental Income",
+      "Dividend Payment",
+      "Bonus Payment",
+    ],
+  },
+  {
+    category: "Savings",
+    descriptions: [
+      "Savings Account Deposit",
+      "Investment Contribution",
+      "Emergency Fund Deposit",
+    ],
+  },
+  {
+    category: "Taxes",
+    descriptions: [
+      "Income Tax Payment",
+      "Property Tax Payment",
+      "Sales Tax Payment",
+      "Tax Consultant Fee",
+    ],
+  },
+  {
+    category: "Retirement",
+    descriptions: [
+      "401(k) Contribution",
+      "IRA Contribution",
+      "Pension Fund Deposit",
+      "Retirement Planning Consultation",
+    ],
+  },
+];
 
 const generateTransactions = (num = 10) => {
   const transactions = [];
@@ -14,15 +146,12 @@ const generateTransactions = (num = 10) => {
     const randomType = transactionTypes[Math.floor(Math.random() * transactionTypes.length)];
     const randomCategory = transactionCategories[Math.floor(Math.random() * transactionCategories.length)];
     const randomDescription = randomCategory.descriptions[Math.floor(Math.random() * randomCategory.descriptions.length)];
-
-    const amount = getRandomAmount(randomDescription);
-
     const transaction = {
       transactionId: faker.random.uuid(),
       category: randomCategory.category,
       type: randomType,
       description: randomDescription,
-      amount: amount,
+      amount: faker.finance.amount(),
       date: faker.date.recent(),
     };
     transactions.push(transaction);
